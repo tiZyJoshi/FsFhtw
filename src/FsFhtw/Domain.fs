0640a30a176016b7f506f5527844017aa79cc039
Johannes Poehn <johannes.poehn@gmail.com>
2021-01-26 17:20:03 +0100

    Final

--- src/FsFhtw/Domain.fs
+++ src/FsFhtw/Domain.fs
@@ -1,7 +1,5 @@
 module Domain
 
-open System
-
 type Player = Player1 | Player2
 
 type PokemonName = Pikachu | Mauzi
@@ -9,16 +7,18 @@ type PokemonName = Pikachu | Mauzi
 type PokemonAction = {
     name : string
     damage : int
+    ep : int
 }
 
 type Pokemon = {
     name : PokemonName
     hp : int
+    ep : int
     actions : PokemonAction list
 }
 
 type ActionResult =
-    Player1ToMove
+    | Player1ToMove
     | Player2ToMove
     | GameWon of Player
 
@@ -37,86 +37,136 @@ let private isGameWonBy player gameState =
 let rec printPokemonActions (actions : PokemonAction list) index =
     match actions with
     | [] -> ()
-    | action::actions -> 
-        printfn "Action (%A): %A" index action.name
-        printfn "Damage: %A" action.damage
+    | action::actions ->
+        let epSign = if action.ep > 0 then $"-{action.ep}" else $"+{action.ep * -1}"
+        printfn $"   {index}: {action.name} (Damage: {action.damage}, EP: {epSign})"
         printPokemonActions actions (index + 1)
-    
+
 let printPlayerActions gameState player =
+    printfn ""
     match player with
     | Player1 ->
-        printfn "You have a %A" gameState.player1Pokemon.name
-        printfn "It has %A HP" gameState.player1Pokemon.hp
+        printfn $"Player1's Turn:\n"
+        printfn $"You have a {gameState.player1Pokemon.name}"
+        printfn $"It has {gameState.player1Pokemon.hp} HP and {gameState.player1Pokemon.ep} EP"
+        printfn $"Please choose your action!\n"
         printPokemonActions gameState.player1Pokemon.actions 0
     | Player2 ->
-        printfn "You have a %A" gameState.player2Pokemon.name
-        printfn "It has %A HP" gameState.player2Pokemon.hp
+        printfn "Player2's Turn:\n"
+        printfn $"You have a {gameState.player2Pokemon.name}"
+        printfn $"It has {gameState.player2Pokemon.hp} HP and {gameState.player2Pokemon.ep} EP"
+        printfn $"Please choose your action!\n"
         printPokemonActions gameState.player2Pokemon.actions 0
-
-let player1Moved gameState action =
-    let newGameState = {
-        player1Pokemon = gameState.player1Pokemon
-        player2Pokemon = {
-            name = gameState.player2Pokemon.name
-            hp = gameState.player2Pokemon.hp - gameState.player1Pokemon.actions.[action].damage
-            actions = gameState.player2Pokemon.actions
-        }}
-
-    printPlayerActions newGameState Player2
-
-    if newGameState |> isGameWonBy Player1 then
-        newGameState, GameWon Player1
-    else 
-        newGameState, Player2ToMove
-
-let player2Moved gameState action =
-    let newGameState = {
-        player1Pokemon = {
-            name = gameState.player1Pokemon.name
-            hp = gameState.player1Pokemon.hp - gameState.player2Pokemon.actions.[action].damage
-            actions = gameState.player1Pokemon.actions
-        }
-        player2Pokemon = gameState.player2Pokemon
-    }
-
-    printPlayerActions newGameState Player1
-
-    if newGameState |> isGameWonBy Player2 then
-        newGameState, GameWon Player2
-    else 
-        newGameState, Player1ToMove
 
 let init () = 
     let gameState = {
         player1Pokemon = {
             name = Pikachu
             hp = 100
+            ep = 50
             actions = [
                 {
-                name = "Hauihaui"
-                damage = 10
+                    name = "Tackle"
+                    damage = 10
+                    ep = 10
                 }
                 {
-                name = "Blitziblitzi"
-                damage = 70
+                    name = "Thunder Wave"
+                    damage = 30
+                    ep = 30
+                }
+                {
+                    name = "Thunderbolt"
+                    damage = 70
+                    ep = 70
+                }
+                {
+                    name = "Rest"
+                    damage = 0
+                    ep = -30
                 }
             ]
         }
         player2Pokemon = {
             name = Mauzi
             hp = 100
+            ep = 60
             actions = [
                 {
-                name = "Kratzikratzi"
-                damage = 25
+                    name = "Tackle"
+                    damage = 10
+                    ep = 10
+                }
+                {
+                    name = "Fake out"
+                    damage = 30
+                    ep = 30
+                }
+                {
+                    name = "Bite"
+                    damage = 60
+                    ep = 60
+                }
+                {
+                    name = "Rest"
+                    damage = 0
+                    ep = -40
                 }
             ]
         }
     }
     printPlayerActions gameState Player1
     printfn ""
-    printfn ""
+
     gameState, Player1ToMove
+
+let player1Moved gameState action =
+    if (List.length gameState.player1Pokemon.actions) <= action then
+        printfn "Please choose a valid action."
+        printPlayerActions gameState Player1
+        gameState, Player1ToMove
+    else
+        if gameState.player1Pokemon.ep < gameState.player1Pokemon.actions.[action].ep then
+            printfn "\nNot enough EP, please try again"
+            printPlayerActions gameState Player1
+            gameState, Player1ToMove
+        else 
+            let newGameState = {
+                player1Pokemon = { gameState.player1Pokemon with ep = gameState.player1Pokemon.ep - gameState.player1Pokemon.actions.[action].ep }
+                player2Pokemon = { gameState.player2Pokemon with hp = gameState.player2Pokemon.hp - gameState.player1Pokemon.actions.[action].damage; ep = gameState.player2Pokemon.ep + 10 }
+            }
+
+            if newGameState |> isGameWonBy Player1 then
+                printfn "GAME WON by %A" Player1
+                newGameState, GameWon Player1
+            else
+                printPlayerActions newGameState Player2
+                newGameState, Player2ToMove
+
+let player2Moved gameState action =
+    if (List.length gameState.player2Pokemon.actions) <= action then
+        printfn "Please choose a valid action."
+        printPlayerActions gameState Player2
+        gameState, Player2ToMove
+    else
+        if gameState.player2Pokemon.ep < gameState.player2Pokemon.actions.[action].ep then
+            printfn "\nNot enough EP, please try again"
+            printPlayerActions gameState Player2
+            gameState, Player2ToMove
+        else 
+            let newGameState = {
+                player1Pokemon = { gameState.player1Pokemon with hp = gameState.player1Pokemon.hp - gameState.player2Pokemon.actions.[action].damage; ep = gameState.player1Pokemon.ep + 10  }
+                player2Pokemon = { gameState.player2Pokemon with ep = gameState.player2Pokemon.ep - gameState.player2Pokemon.actions.[action].ep }
+            }
+                
+            if newGameState |> isGameWonBy Player2 then
+                printfn "GAME WON by %A" Player2
+                newGameState, GameWon Player2
+            else
+                printPlayerActions newGameState Player1
+                newGameState, Player1ToMove
+
+
 
 let update (msg : Message) model =
     let (gameState, actionResult) = model
@@ -131,5 +181,4 @@ let update (msg : Message) model =
         //printPlayerActions gameState Player2
         player2Moved gameState action
     | GameWon player ->
-        printfn "GAME WON by %A" player
         gameState, GameWon player
--- src/FsFhtw/Program.fs
+++ src/FsFhtw/Program.fs
@@ -1,8 +1,53 @@
 [<EntryPoint>]
 let main argv =
+    System.Console.OutputEncoding <- System.Text.Encoding.UTF8
+    System.Console.WindowHeight <- 50
+    System.Console.WindowWidth <- 130
     printfn "Welcome to the FHTW Domain REPL!"
     printfn "Please enter your commands to interact with the system."
     printfn "Press CTRL+C to stop the program."
+    printfn "A bad clone of"
+
+    printfn "
+                                                                    .;1.
+                                                                   .i11i
+                                                                  .i111t:
+                                                                 ,i11tt11,
+                                                                ,i11f;tt11.
+                                                               ,111f;;ft1t:
+                                                .,;;.;i       .111fLftt1;.          .,,,,,,.
+                   .,:::::,.                 ,:i11ti:11i.    .i11tft1i;.     .,,,,,;1t1111t1
+               .:ii111111111i:.          .,;i111111ii1111,  .;:i11111:.    .;111111111111111.       ,:.
+            .:i1111111111111111:       .,i11111tt11i11t111:  ,:i1111111i;. :i111111111tfff11,     .:;t11i;:.
+          .:11111ttfffffffft1111;      :;i1tff;;f1111fft111;.:111111111111::;11fffft11fLLf1t:     .;;111ttt11i;.
+         :1111tf;;;LLLLLLLLfft111:     :;i1ffLLLf111fLLftt111111tffffftt111ii1tLLLLt11ffLf1t;     .;i1tftt111tt::1i:,.
+       .i111ff;;LLLLLLLLLLLLLft111.    :ii111ffLft1tLffLLft111t;;;;L;;Lftt1111tLfLLf1tLLLft1i     .ii1tffffft11iitttt1i;:.
+      .:;11f;LLLLLLLLLLLLLLLfLft11;    :11111fLLf1tLfLLfLLt11f;LfttttfLL;t1111fLLLLf1tLfLft11     .ii111LLLLft1111t111ttt;
+      .::i1tLfLLLLLLLLL;fffL;fLf11i    :;::i1fLLftfLLLL;ft11tLLf11i11tLLt11i11fLLLLfttLLLLt11. ,:;i1i;itfLLLft111tfffft11.
+       ,;;11tLfLLLLLLLft1111f;Lft1i   .,.::i1fLLffLLLLLft111fL;t1..it;;t11i:11fLfLLLtfLLLLt11;i1ttt1111tLfLLLt111fLLLLt1i
+        ;;i11fLfLLLLLLf11i111fLft1i.:i1111111tLfLLLLLLft1i11fL;ti.it;Lt111ii1tLLLLLLfLLLLLf1111tttttt111fLLLLf111fLfLf1t:
+        .i;i1tt1fLfLLLLt1:.11fLf11ii11ttt1111tLfLLLLLf11: 11;Lft1it;Lt11t1111tLfLLLLLLLLfLf111ffttfLftt11fLfLft1tfLfLf11.
+         :ii11111LLLLLLf1i.i1fLf1111t;ftfftt11fLLLLLf111;,11ffLf1tfLt1tfft111tLLLLLLLLLLLLt1tff11fLffftt1tLfLft1tLfLftti
+          iii1i11fLfLLLf11;itLLt11tf;t1fLLLft11fLLLLtt111111fLLff;LftfLLLft111tLLffLLLLLLf11fLt11fLLtfft1tLfLft1fLfLf1t,
+          ,1iiii11fLLLLLt111fLf11tLLtitLLftff11tLfLLffftt1111fLLLLLfLLLfLLf111fLLttLfLffLt1tLf1111tt1fLt1tfLfLt1fLfLt11
+           :,,i;11fLfLfLf1tfLf111fLf111tff1fft1tLLLLLLLLfft11tfLLLLLLLLLft111fLLft1fLLtff11fLft11111tfLt1tLffLftLfLftt;
+              ;;i11LLLLLff;Lf111tLLf1111111fft1tLfttfLLLLLft111tfLLL;fft1111tLfLft1tLf1ff11ffLft111tfLLt1tLtfLffLf;f1t,
+              .;;11fLfLLLLft1111tLLft11111fLft1tLf111tfLLLLtt11111ttt1111ii1tLfLf111ft1tf11fLLLfftfLLLft1ff1fLLLLLLt11
+               :;i11LLLLLft11ii1fLfLfttttfLLft1tLf11111tffLLftt1111111i;::i1fLLLf111tt1tf11tLfLLLLLLfLt1tLf1fLLLLLf1t;
+               .;;11fLLLLf111;i1tLfLLLffLLfLf11fLf11i;i111tfLLfftt11:,. ,:11ttfft1111111ft11fLLLLLLLLf11fLt1fLLLfLf11.
+                ::;11LLLLft11;;11LfLLLLLLfLLt1tLLf11;:;;i111tfLLLt11.   :;11111111ii1111ft111fL;;L;ft11fLft1fLLLLLt1i
+                .::11fLLLLt11i:i1tLLLLLLLL;t11tLLf11: ,:;;i111tfLt11.   ::;;;iii11;;1ii1fft111ttttt111fLLft1fLLLLf1t:
+                 ::;11LLfLf11i::11tfLL;;Lf1111tLL;11:   ,:;;i111tt11.      ..,,::.:;i;i1tLfft111111ii1fLLf11fLLLLt11.
+                 .::11fLfLft11::;111tttt111i;1tfft11:     .:;;;i1111.             .,,;i11tttt11i:,,:i1ffLf11fLfLftti
+                  ::;11LLLft11,,::ii11111i:::1111111:       .::;;i11.               .;;i11111111  ,;i111tt11fLfLf1t,
+                  .::11fLfLf11; .:::;;;;,  ::iii;;:.          .,:;;i                 ,,::;;iii11. ,;iii11111fLfLt11
+                   ::;11LfLft1i   ..,..    ,,...                 ,,                      ..,,::.  ..,,::;;i1fLLftt;
+                   .::11f;ff111,                                                                        .;itttff1t,
+                    ::;11t11111,                                                                        .;;111111i
+                    .::1111i;:.                                                                          ,:;;ii1t:
+                     ::;i;,.                                                                               .,::;:.
+                     ,:..                                                                                     ..
+    "
     
     let initialState = Domain.init ()
     printf "> "
